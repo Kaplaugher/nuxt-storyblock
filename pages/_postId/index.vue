2aa772729e30fb5edeacf27e82b58381581000e6
Kyle Plaugher <kaplaugher94@gmail.com>
2018-04-12 18:31:28 -0400

    updates

--- pages/_postId/index.vue
+++ pages/_postId/index.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    <h1>post id page</h1>
+  <div id="post">
+    <div class="post-thumbnail" :style="{backgroundImage: 'url(' + image + ')'}"></div>
+    <h1>{{title}}</h1>
+    <p>{{content}}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  asyncData(context) {
+    return context.app.$storyapi.get(`cdn/stories/blog/${context.params.postId}`, {
+      version: 'draft'
+    }).then(res => {
+      return {
+        image: res.data.story.content.thumbnail,
+        title: res.data.story.content.title,
+        content: res.data.story.content.content
+      }
+    })
+  }
 }
 </script>
 
 <style>
+.post-thumbnail {
+  width: 100;
+  height: 300px;
+  background-size: cover;
+  background-position: center;
+}
 
 </style>
